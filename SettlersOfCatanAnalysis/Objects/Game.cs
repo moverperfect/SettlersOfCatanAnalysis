85d93e2cc20544f3183235ffdf3a252ca7ef326c
moverperfect <moverperfect@gmail.com>
2017-07-04 18:34:46 +0100

    Added the main game object

---
+++ SettlersOfCatanAnalysis/Objects/Game.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+
+namespace SettlersOfCatanAnalysis.Objects
+{
+    internal class Game
+    {
+        /// <summary>
+        /// Indicates whether we are playing with a randomly generated board or not
+        /// </summary>
+        private readonly bool _randomBoard;
+
+        /// <summary>
+        /// The board that this game is playing on
+        /// </summary>
+        internal Board Board { get; private set; }
+
+        /// <summary>
+        /// A list of the current players in the game
+        /// </summary>
+        internal List<Player> Players { get; }
+
+        /// <summary>
+        /// Initiate a game
+        /// </summary>
+        /// <param name="randomBoard">Is the board in this game random</param>
+        internal Game(bool randomBoard)
+        {
+            Players = new List<Player> {new Player(false, Player.Colour.Blue), new Player(false, Player.Colour.Orange)};
+            _randomBoard = randomBoard;
+            InitiateBoard(_randomBoard);
+        }
+
+
+        private bool InitiateBoard(bool randomBoard)
+        {
+            Board = new Board(randomBoard);
+
+            return true;
+        }
+
+        internal bool Start()
+        {
+            var gameComplete = false;
+
+            // Place initial settlements
+            for (var i = 0; i < Players.Count; i++)
+            {
+                Players[i].StartSettlement(Board);
+            }
+
+            // While we are still going
+            while (!gameComplete)
+            {
+                for (var i = 0; i < Players.Count; i++)
+                {
+                    
+                }
+            }
+            return true;
+        }
+    }
+}
--- SettlersOfCatanAnalysis/Program.cs
+++ SettlersOfCatanAnalysis/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using SettlersOfCatanAnalysis.Objects;
 
 namespace SettlersOfCatanAnalysis
 {
@@ -8,6 +9,9 @@ namespace SettlersOfCatanAnalysis
         {
             Console.WriteLine("Welcome to this Analysis of Settlers of Catan");
             Console.WriteLine("Written in C# By Jack Moorhouse advised by Eleanor Partington");
+
+            var game = new Game(true);
+            game.Start();
         }
     }
 }
